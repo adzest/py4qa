68065c54d3cd944121e6b3946ce523fb5dd9177c
Oleksii Isakov <isakov.ktk@gmail.com>
2018-03-26 21:09:13 +0300

    minor updates.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,7 +4,6 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(name='testN', header='testH', footer='testF'))
-    app.session.logout()
 
 
 def test_add_empty_group(app):
