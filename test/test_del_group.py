db364c9b47cdcdb55d030678821509a846da9bab
Oleksii Isakov <isakov.ktk@gmail.com>
2018-03-27 18:53:18 +0300

    update existed tests.

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -3,4 +3,6 @@ from model.group import Group
 
 
 def test_add_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name='test_del_new'))
     app.group.delete_first_group()
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,10 +3,7 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
+    #ToDo - ?: add precondition
     app.group.modify_first_group(Group(name='modified_'))
 
-def test_modify_group_header(app):
-    app.group.modify_first_group(Group(header='modified_'))
 
-def test_modify_group_footer(app):
-    app.group.modify_first_group(Group(footer='modified_'))
