481a4140614dbc460e6f26d1939f442bfbf6e699
Oleksii Isakov <isakov.ktk@gmail.com>
2018-04-03 10:56:11 +0300

    extend contact model, add cooments to fields

--- model/contact.py
+++ model/contact.py
@@ -3,7 +3,7 @@ class Contact:
     def __init__(self, firstname=None, middlename=None, lastname=None, nickname=None, title=None, company=None,
                  address=None, photo=None, homephone=None, mobilephone=None, workphone=None, fax=None, email=None,
                  email2=None, email3=None, homepage=None, bday=None, bmonth=None, byear=None, aday=None, amonth=None,
-                 ayear=None):
+                 ayear=None, address2=None, text=None, notes=None):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
@@ -11,8 +11,7 @@ class Contact:
         self.title = title
         self.company = company
         self.address = address
-        # file path
-        self.photo = photo
+        self.photo = photo  # file path
         self.homephone = homephone
         self.mobilephone = mobilephone
         self.workphone = workphone
@@ -20,10 +19,13 @@ class Contact:
         self.email = email
         self.email2 = email2
         self.email3 = email3
-        self.homepage = homepage
-        self.bday = bday
-        self.bmonth = bmonth
+        self.bday = bday  # Select intl
+        self.bmonth = bmonth  # Select str
         self.byear = byear
-        self.aday = aday
-        self.amonth = amonth
+        self.aday = aday  # Select int
+        self.amonth = amonth  # Select str
         self.ayear = ayear
+        # Secondary section
+        self.address2 = address2
+        self.text = text
+        self.notes = notes
